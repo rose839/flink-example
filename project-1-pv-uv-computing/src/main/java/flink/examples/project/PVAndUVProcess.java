ca438e9e74f0d87371dfd7e260f3af3b44e07be4
rose839 <617418745@qq.com>
2021-07-14 17:19:19 +0000

    refactor: add watermark class

--- project-1-pv-uv-computing/src/main/java/flink/examples/project/PVAndUVProcess.java
+++ project-1-pv-uv-computing/src/main/java/flink/examples/project/PVAndUVProcess.java
@@ -2,7 +2,15 @@ package flink.examples.project;
 
 import java.util.Properties;
 
+import org.apache.flink.api.common.eventtime.*;
+import org.apache.flink.api.common.eventtime.WatermarkOutput;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer010;
 
 public class PVAndUVProcess {
     public static void main(String[] args) {
@@ -12,6 +20,44 @@ public class PVAndUVProcess {
         System.out.printf("Reading from kafka topic %s @ %s\n", kafkaTopic, brokers);
 
         Properties kafkaProps = new Properties();
+        kafkaProps.setProperty("bootstrap.servers", brokers);
+        FlinkKafkaConsumer010<UserBehaviorEvent> kafka = new FlinkKafkaConsumer010<>(kafkaTopic, new UserBehaviorEventSchema(), kafkaProps);
+        kafka.setStartFromLatest();
+        kafka.setCommitOffsetsOnCheckpoints(false);
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+
+        kafka.assignTimestampsAndWatermarks(
+            WatermarkStrategy
+                .forGenerator((ctx) -> new PeriodicWatermarkGenerator())
+                .withTimestampAssigner((ctx) -> new TimeStampExtractor()));
+
+        DataStreamSource<UserBehaviorEvent> dataStreamByEventTime = env.addSource(kafka);
         
+        DataStream<Tuple4<Long, Long, Long, Integer>> 
+    }
+
+    private static class PeriodicWatermarkGenerator implements WatermarkGenerator<UserBehaviorEvent>, Serializable {
+        private long currentWatermark = Long.MIN_VALUE;
+
+        @Override
+        public void onEvent(UserBehaviorEvent event, long eventTimestamp, WatermarkOutput output) {
+            currentWatermark = eventTimestamp;
+        }
+
+        @Override
+        public void onPeriodicEmit(WatermarkOutput output) {
+            long effectiveWatermark =
+                    currentWatermark == Long.MIN_VALUE ? Long.MIN_VALUE : currentWatermark - 1;
+            output.emitWatermark(new Watermark(effectiveWatermark)); 
+        }
+    }
+
+    private static class TimeStampExtractor implements TimestampAssigner<UserBehaviorEvent> {
+        @Override
+        public long extractTimestamp(UserBehaviorEvent element, long recordTimestamp) {
+            return element.getTs();
+        }
     }
 }
---
+++ project-1-pv-uv-computing/src/main/java/flink/examples/project/UserBehaviorEventSchema.java
@@ -0,0 +1,28 @@
+package flink.examples.project;
+
+import com.alibaba.fastjson.JSON;
+import org.apache.flink.api.common.serialization.DeserializationSchema;
+import org.apache.flink.api.common.serialization.SerializationSchema;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+
+public class UserBehaviorEventSchema implements SerializationSchema<UserBehaviorEvent>, DeserializationSchema<UserBehaviorEvent> {
+    @Override
+    public byte[] serialize(UserBehaviorEvent event) {
+        return event.toString().getBytes();
+    }
+
+    @Override
+    public UserBehaviorEvent deserialize(byte[] message) throws IOException {
+        return JSON.parseObject(message, UserBehaviorEvent.class);
+    }
+
+    @Override
+    public boolean isEndOfStream(UserBehaviorEvent nextElement) {
+        return false;
+    }
+
+    @Override
+    public TypeInformation<UserBehaviorEvent> getProducedType() {
+        return TypeInformation.of(UserBehaviorEvent.class);
+    }
+}
