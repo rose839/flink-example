0721273108814d32f88588c921fcc430cd5dd042
rose839 <617418745@qq.com>
2021-07-20 17:07:44 +0000

    feat: add source from collection

---
+++ datastream-api/src/main/java/flink/examples/source/StreamingFromCollection.java
@@ -0,0 +1,7 @@
+package flink.examples.source;
+
+public class StreamingFromCollection {
+    public static void main(String[] args) {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+    }
+}
