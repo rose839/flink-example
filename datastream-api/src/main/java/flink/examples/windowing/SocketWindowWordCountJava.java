d90fdf91efb325339c92e04e183efdca380502b4
rose839 <617418745@qq.com>
2021-07-27 00:33:29 +0800

    add window proc

---
+++ datastream-api/src/main/java/flink/examples/windowing/SocketWindowWordCountJava.java
@@ -0,0 +1,74 @@
+package flink.examples.windowing;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.util.Collector;
+
+public class SocketWindowWordCountJava {
+    public static void main(String[] args) throws Exception{
+        int port;
+        try {
+            ParameterTool parameterTool = ParameterTool.fromArgs(args);
+            port = parameterTool.getInt("port");
+        }catch (Exception e){
+            System.err.println("No port set. use default port 9000--java");
+            port = 9000;
+        }
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        String hostname = "hadoop100";
+        String delimiter = "\n";
+
+        DataStreamSource<String> text = env.socketTextStream(hostname, port, delimiter);
+
+        // a a c
+
+        // a 1
+        // a 1
+        // c 1
+        DataStream<WordWithCount> windowCounts = text.flatMap(new FlatMapFunction<String, WordWithCount>() {
+            @Override
+            public void flatMap(String value, Collector<WordWithCount> out) throws Exception {
+                String[] splits = value.split("\\s");
+                for (String word : splits) {
+                    out.collect(new WordWithCount(word, 1L));
+                }
+            }
+        }).keyBy("word")
+                .timeWindow(Time.seconds(2), Time.seconds(1))//指定时间窗口大小为2秒，指定时间间隔为1秒
+                .sum("count");//在这里使用sum或者reduce都可以
+                /*.reduce(new ReduceFunction<WordWithCount>() {
+                                    public WordWithCount reduce(WordWithCount a, WordWithCount b) throws Exception {
+
+                                        return new WordWithCount(a.word,a.count+b.count);
+                                    }
+                                })*/
+
+        windowCounts.print().setParallelism(1);
+
+        env.execute("Socket window count");
+
+    }
+
+    public static class WordWithCount{
+        public String word;
+        public long count;
+        public  WordWithCount(){}
+        public WordWithCount(String word,long count){
+            this.word = word;
+            this.count = count;
+        }
+        @Override
+        public String toString() {
+            return "WordWithCount{" +
+                    "word='" + word + '\'' +
+                    ", count=" + count +
+                    '}';
+        }
+    }
+}
